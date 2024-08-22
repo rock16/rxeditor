49dfdb86b890e052dc1df5ef17bd9ff18689590b
Rock <rockok10@gmail.com>
2024-08-22 07:46:27 +0000

    Add undo features

--- src/lib/document.rs
+++ src/lib/document.rs
@@ -1,6 +1,8 @@
 pub struct Document {
     content: String,
     cursor_position: usize,
+    undo_stack: Vec<String>,
+    redo_stack: Vec<String>,
 }
 
 impl Document {
@@ -8,16 +10,20 @@ impl Document {
         Self {
             content: String::new(),
             cursor_position: 0,
+            undo_stack: Vec::new(),
+            redo_stack: Vec::new(),
         }
     }
 
     pub fn insert_text(&mut self, text: &str, position: usize){
+        self.undo_stack.push(self.content.clone());
         self.content.insert_str(position,text);
         self.cursor_position += text.len();
     }
 
     pub fn remove_text(&mut self, start: usize, end: usize){
         if end >= start{
+            self.undo_stack.push(self.content.clone());
             self.content.drain(start..end);
             self.cursor_position = start.min(self.content.len());
         }
@@ -31,5 +37,24 @@ impl Document {
         self.cursor_position
     }
 
+    pub fn undo(&mut self){
+        if let Some(previous_content)=self.undo_stack.pop(){
+            self.redo_stack.push(self.content.clone());
+            self.content = previous_content;
+    }
 }
+#[cfg(tests)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_insert_text(){
+        let mut document = Document::new();
+        document.insert_text("Hello ",0);
+        document.insert_text("world",5);
+
+        assert_equal(document.get_content(), "Hello world");
+    }
+}
+
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod lib::document;
 fn main() {
     println!("Hello, world!");
 }
